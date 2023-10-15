2060504cab727bb976d0684733cb6b3dbf917c13
ogiamien emmanuel tammy <ogiamien222@gmail.com>
2023-10-15 09:24:16 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,88 @@
 import './todo.css';
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 function App() {
   const [todo, setTodo] = useState([]);
-  const [values, setValues] = useState("");
+  const [values, setValues] = useState('');
+  const [value, setValue] = useState('')
 
   const valueShown = (e) => {
     setValues(e.target.value);
   };
 
+  const valuesShown = (e) => {
+    setValue(e.target.value)
+  }
+
   const clearField = () => {
-    setValues("");
+    setValues('');
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setTodo([...todo, values]);
-    console.log(todo);
-    clearField();
+    if (values.trim() === '') {
+      return; // Prevent adding empty todos
+    }
+
+    if (values !== '') {
+      setTodo([values, ...todo]);
+      clearField();
+    }
   };
 
   const deleteTodo = (index) => {
-      setTodo(prevTodo => prevTodo.filter((_, i) => i !== index));
+    const updatedTodo = [...todo];
+    updatedTodo.splice(index, 1);
+    setTodo(updatedTodo);
   };
 
-  const editTodo = (index) => {
-    setTodo(prevTodo => prevTodo.map)
+  const [editIndex, setEditIndex] = useState(-1); // Initialize with -1 to indicate no editing.
+
+  const startEdit = (index) => {
+      setEditIndex(index);
+      setValue(todo[index]);
+  };
+
+  const saveEdit = (index) => {
+    const updatedTodo = [...todo];
+    updatedTodo[index] = value;
+    setTodo(updatedTodo);
+    setEditIndex(-1); // Reset editIndex to -1 after editing.
+    clearField();
+  };
+
+  const cancelEdit = () => {
+    setEditIndex(-1)
   }
 
   return (
-    <div>
+    <div className='formss'>
       <h1>hello world</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={valueShown} value={values} />
-        <button type="submit" disabled={values.trim() === ""}>
-          Submit
-        </button>
-
-        {todo.map((list, index) => (
-          <div key={index}>
-            <div className="todos">
-              <div>{list}</div>
-              <button onClick={() => deleteTodo(index)}>Delete</button>
+      <form onSubmit={handleSubmit} className='inputs'>
+        <input className='inputfields' type="text" onChange={valueShown} value={values} />
+        <button type="submit" className='inputfields'>Submit</button>
+        </form>
+      {todo.map((list, index) => (
+        <div key={index}>
+          {editIndex === index ? (
+            <div>
+              <input className='inputfield' type="text" value={value} onChange={valuesShown} />
+              <button onClick={() => saveEdit(index)}>Save</button>
+              {/* <button onClick={() => deleteTodo(index)}>Delete</button> */}
+              <button onClick={() => cancelEdit(index)}>Cancel</button>
             </div>
-
-          </div>
-        ))}
-      </form>
+          ) : (
+            <div className='todo'>
+              <div>{list}</div>
+                <div>
+                <button onClick={() => startEdit(index)}>Edit</button>
+                <button onClick={() => deleteTodo(index)}>Delete</button>
+                </div>
+            </div>
+          )}
+        </div>
+      ))}
+       
     </div>
   );
 }
